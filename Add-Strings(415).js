56e0928a545eaaa810280cf82308bf406d6b857f
Narek <narekazibekyan5@gmail.com>
2023-07-01 18:27:46 +0400

    Add Strings(415)

---
+++ Add-Strings(415).js
@@ -0,0 +1,5 @@
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+var addStrings = function (num1, num2) {
+  return +num1 + +num2;
+};
