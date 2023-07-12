397c75a8917ca030a9360e3be1a15ab53afaac05
Narek <narekazibekyan5@gmail.com>
2023-07-12 20:14:46 +0400

    Single Number(136)

---
+++ Single-Number(136).js
@@ -0,0 +1,12 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+var singleNumber = function (nums) {
+  nums = nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i + 1] !== nums[i] && nums[i - 1] !== nums[i]) {
+      return nums[i];
+    }
+  }
+};
